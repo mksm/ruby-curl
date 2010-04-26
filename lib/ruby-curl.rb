1a6e72f77cf652ebd668e65fe40cee495ff7f5af
Ricardo Amorim <mksm.sama@gmail.com>
2010-04-26 15:41:31 -0300

    Added, renamed and deleted files; usage: require 'lib/ruby-curl'

---
+++ lib/ruby-curl.rb
@@ -0,0 +1,10 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'rubygems'
+require 'rack/utils'
+
+require 'rb_curl'
+
+require 'ruby-curl/options'
+require 'ruby-curl/easy'
+require 'ruby-curl/multi'
